873aeeb81c629a01eb078a1fb0788a885336c3b7
swarminglogic <github@swarminglogic.com>
2013-07-01 21:07:19 +0200

    Fixed bug in CodeIgniter's captcha helper
    Fixed bug in CodeIgniter's captcha helper. It now only removes image-files in
    the captcha folder. This explained the mysterious deletion of
    `images/captcha/folder-info.txt`.
    
    Note that strict comparison is used, !(strpos($filename, ".jpg") === false)).
    It's not really necessary, but it would cause problesm is ".jpg" was a valid
    filename, as it would return 0 as the position, and would evaluate to false.
    
      system/helpers/captcha_helper.php

--- system/helpers/captcha_helper.php
+++ system/helpers/captcha_helper.php
@@ -89,7 +89,7 @@ if ( ! function_exists('create_captcha'))
 
 		while ($filename = @readdir($current_dir))
 		{
-			if ($filename != "." and $filename != ".." and $filename != "index.html")
+			if ($filename != "." and $filename != ".." and !(strpos($filename, ".jpg") === false))
 			{
 				$name = str_replace(".jpg", "", $filename);
 
