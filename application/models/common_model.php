147fd0595b4e63937fb451e42f7929131b0a973e
swarminglogic <github@swarminglogic.com>
2013-08-17 20:31:56 +0200

    Added base shell for watermark article.
    Added base shell for watermark article.
    To be merged in master when finished.
        application/models/common_model.php
        application/views/jottings/2013_08_watermark.php   (+)

--- application/models/common_model.php
+++ application/models/common_model.php
@@ -7,12 +7,16 @@ class Common_model extends CI_Model {
 
 
   public function get_keyword_description() {
+    $keydesc['automization'] =
+      "Topics related to doing things slowly once, to avoid spending time doing it again later.";
     $keydesc['bash'] =
       "Uses scripts written for the bash unix terminal.";
     $keydesc['c++'] =
       "Topics relating to the C++ programming language";
     $keydesc['curl'] =
       "Uses unix tool 'curl' to retrieve web page sources";
+    $keydesc['imagemagick'] =
+      "Collection of command-line tools for image manipulation.";
     $keydesc['linux'] =
       "Article is more relevant for linux based users";
     $keydesc['math'] =
@@ -23,6 +27,8 @@ class Common_model extends CI_Model {
       "Software construction tool (think: make)";
     $keydesc['visualization'] =
       "Covers some aspect of data visualization.";
+    $keydesc['watermark'] =
+      "On watermarks in images.";
     return $keydesc;
   }
 }
---
+++ application/views/jottings/2013_08_watermark.php
@@ -0,0 +1,36 @@
+<?php
+$languages = array("Bash", "Plain");
+$data['lang'] = $languages;
+$this->load->view('parser/code', $data);
+$data['cclicense'] = 'by';
+?>
+
+<div>
+  <div id="article_page" class="twelve columns" data-target="#toc">
+    <?php heading(2, 'Automatic Watermarking of Images','Top',false); ?>
+    <p>
+    </p>
+
+    <?php heading(4, '1. Overview'); ?>
+    <p>
+    </p>
+
+    <?php heading(4, '2. Configuring Fonts for <code>ImageMagick</code>', '2. Configuring Fonts'); ?>
+
+    <?php heading(4, '3. Simple Examples'); ?>
+    <p>
+    </p>
+
+    <?php heading(4, '4. Bash script'); ?>
+    <p>
+    </p>
+
+    <?php heading(4, '5. Thoughts & Conclusions'); ?>
+    <p>
+    </p>
+  </div>
+  <?php
+  global $toc;
+  $data['toc'] = $toc;
+  $this->load->view('sidebars/sb_jotting', $data); ?>
+</div>
